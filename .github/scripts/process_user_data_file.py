da6deb2a65780827dab07cbeb295aa90ed69fb51
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-04-25 11:35:47 -0700

    revert pre commit config

--- .github/scripts/process_user_data_file.py
+++ .github/scripts/process_user_data_file.py
@@ -5,15 +5,14 @@ from jinja2 import Template
 
 ssm = boto3.client("ssm")
 
+# Retrieve Windows Administrator Password from AWS SSM
 password = ssm.get_parameter(
     Name="/windows/commando/administrator/password", WithDecryption=True
 )["Parameter"]["Value"]
 
-print("[Debugging] Password: ", password)
-
 user_data_file_location = "./src/winrm_bootstrap.txt"
 with open(user_data_file_location) as user_data_file:
-    # Process user data file with the Windows Administrator password
+    # Process the User Data File with the Windows Administrator Password
     Template(user_data_file.read()).stream(password=password).dump(
         user_data_file_location
     )

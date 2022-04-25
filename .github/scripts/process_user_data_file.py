a69361081263899d9bf55120a3e39c91007bd56f
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-04-25 11:20:57 -0700

    fix: decrypt ssm password in user data file

--- .github/scripts/process_user_data_file.py
+++ .github/scripts/process_user_data_file.py
@@ -5,17 +5,15 @@ from jinja2 import Template
 
 ssm = boto3.client("ssm")
 
-password = ssm.get_parameter(Name="/windows/commando/administrator/password")[
-    "Parameter"
-]["Value"]
+password = ssm.get_parameter(
+    Name="/windows/commando/administrator/password", WithDecryption=True
+)["Parameter"]["Value"]
 
 print("[Debugging] Password: ", password)
-
-secret = "SuperS3cr3t!!!!"
 
 user_data_file_location = "./src/winrm_bootstrap.txt"
 with open(user_data_file_location) as user_data_file:
     # Process user data file with the Windows Administrator password
-    Template(user_data_file.read()).stream(password=secret).dump(
+    Template(user_data_file.read()).stream(password=password).dump(
         user_data_file_location
     )

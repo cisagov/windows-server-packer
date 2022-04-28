a2588526ee4f434694eba5849472491f3b65d7f1
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-04-28 11:01:18 -0700

    capitalization fixes and comment updates to process_user_data_file script

--- .github/scripts/process_user_data_file.py
+++ .github/scripts/process_user_data_file.py
@@ -1,20 +1,22 @@
-"""Process User Data File."""
+"""Process user data file."""
 # Third-Party Libraries
 import boto3
 from jinja2 import Template
 
 ssm = boto3.client("ssm")
 
-# Retrieve Windows Administrator Password from AWS SSM
+# Retrieve Windows Administrator password from AWS SSM
 password = ssm.get_parameter(
     Name="/windows/commando/administrator/password", WithDecryption=True
 )["Parameter"]["Value"]
 
 user_data_file_location = "./src/winrm_bootstrap.txt"
 with open(user_data_file_location) as user_data_file:
-    # Process the User Data File with the Windows Administrator Password
+    # Process the user data file with the Windows Administrator password
     Template(user_data_file.read()).stream(password=password).dump(
         user_data_file_location
     )
 
+# This password is masked from logging when run via GH Actions.
+# But beware if this script is running elsewhere.
 print(password)

5cf55da3540c0993c27dc31ab95aa0589a8bf737
Kyle <kylehanks@pm.me>
2022-05-24 22:48:47 -0700

    setup powershell provisioners to disable windows defender and enable RDP service

---
+++ src/powershell/disable-defender.ps1
@@ -0,0 +1,14 @@
+# check for existence windows devender
+Write-Output "[ ] Checking if Windows Defender is running"
+Get-Service -Name WinDefend -ErrorVariable err -ErrorAction SilentlyContinue
+if ($err) {
+    Write-Output "[ ] Windows Defender not found, skipping removal"
+    exit 1
+}
+Write-Output "[*] Windows Defender found"
+
+# uninstall windows defender service
+Write-Output "[ ] Attempting to uninstall Windows Defender"
+Uninstall-WindowsFeature -Name Windows-Defender -ErrorAction stop
+Write-Output "[*] Successfully ran uninstall command"
+Write-Output "[ ] Rebooting and then checking if uninstall was successful"
---
+++ src/powershell/enable-rdp.ps1
@@ -0,0 +1,27 @@
+Write-Output "[ ] Attempting to enable RDP service"
+
+$name = "fDenyTSConnections"
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server"
+Write-Output "[ ] Configuring setting: $name"
+Set-ItemProperty -Path $path -Name $name -Value "0"
+if ($(Get-ItemProperty -Path $path -Name $name).fDenyTSConnections -ne 0) {
+    Write-Error "[X] Failed to verify setting: $name" -ErrorAction Stop
+}
+Write-Output "[*] Setting successfully verified: $name"
+
+$name = "UserAuthentication"
+$path = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
+Write-Output "[ ] Configuring setting: $name"
+Set-ItemProperty -Path $path -Name $name -Value "1"
+if ($(Get-ItemProperty -Path $path -Name $name).UserAuthentication -ne 1) {
+    Write-Error "[X] Failed to verify setting: $name" -ErrorAction Stop
+}
+Write-Output "[*] Setting successfully verified: $name"
+
+$name = "RemoteDesktop-In-TCP-WS"
+Write-Output "[ ] Configuring firewall rule: $name"
+Set-NetFirewallRule -Name $name -Enabled True
+if ($(Get-NetFirewallRule -Name $name).Enabled -ne $true) {
+    Write-Error "[X] Failed to verify firewall rule: $name" -ErrorAction Stop
+}
+Write-Output "[*] Firewall rule successfully verified: $name"
